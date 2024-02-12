d1b3cfcfe74e1cd71971fde25abcfaa57923f91a
WebGCDev <gaetano.chi@gmail.com>
2024-02-12 19:55:37 +0100

    Add: show category in info if present

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -2,9 +2,11 @@
 
 @section('content')
   <ul >
-    
+        {{-- <li>{{$project->type?->title}}  nullish operatore --}}
+
         <li>Titolo: {{$project->title}}</li>
         <li>Autore: {{$project->author}}</li>
+        <li>Categoria: {{$project->type?->title}}</li>
         <li>Data creazione: {{$project->creation_date}}</li>
         <li>Ultimo commit: {{$project->last_update}}</li>
         <li>Linguaggi di programmazione: {{$project->lang}}</li>
